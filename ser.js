1be49f4e1fef9d457d01829cddfa1fbd09d0aa8b
MrkThx <100205253+MrkThx@users.noreply.github.com>
2022-05-11 23:02:29 +0800

    Update ser.js

--- ser.js
+++ ser.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const app = express()
-const port = 1722
+const port = 1723
 
 app.listen(port, () =>{
     console.log(`listening on port: ${port}`)
